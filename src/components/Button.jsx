09491359a432f1787e7ae327e207ff107fb84719
Esumei Moses <archer.me24@gmail.com>
2023-05-06 09:52:03 +0100

    Homepage first commit

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,9 +1,18 @@
-import React from 'react';
+import PropTypes from 'prop-types';
 
-const Button = () => {
+//passing props from parent to child component ensures reusability across multiple places with diff style features
+const Button = ({ bgColor, color, size, text, borderRadius }) => {
     return (
-        <div>Button</div>
+        <button type="button" style={{ backgroundColor: bgColor, color, borderRadius }} className={`text-${size} p-3 hover:drop-shadow-xl`}>Button</button>
     )
+}
+//eslint requirement not compulsory though
+Button.propTypes = {
+    bgColor: PropTypes.string.isRequired,
+    color: PropTypes.string.isRequired,
+    size: PropTypes.string.isRequired,
+    text: PropTypes.string.isRequired,
+    borderRadius: PropTypes.string.isRequired
 }
 
 export default Button;
